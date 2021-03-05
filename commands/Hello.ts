dfee600e85bb67382e51b5ab160e4089d5572ba5
titancube <kbin0717@gmail.com>
2021-03-06 03:06:15 +0900

    chore: delete unused files

--- commands/Hello.ts
+++
@@ -1,8 +0,0 @@
-import { Command, CommandMessage } from "@typeit/discord";
-
-export abstract class Hello {
-  @Command("hello")
-  async hello(command: CommandMessage) {
-    command.reply("Hello!");
-  }
-}
