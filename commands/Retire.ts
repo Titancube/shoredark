81e05812f6c67595183f5afa84ae01a5a58abbd8
titancube <kbin0717@gmail.com>
2021-03-07 07:52:37 +0900

    test(feat): show ramaining hours and minutes

--- commands/Retire.ts
+++ commands/Retire.ts
@@ -9,29 +9,43 @@ export abstract class Hello {
         command.channel.send(await this.getRetireDay())
     }
 
+    // thanks to 7OAST
+    showDischargeDate(date: number | Date): string {
+        if (date < 86400) {
+            if (date < 3600) {
+                const resultTime = Math.floor(parseInt(date.toString()) / 24);
+                const remainder = parseInt(date.toString()) % 24;
+
+                return `${resultTime}시간 ${remainder}분`
+            }
+            else {
+                format(date, 'yyyy년 MM월 dd일')
+            }
+        }
+    }
+
     async getRetireDay(): Promise<string> {
         const r = await db.collection('Retire').get()
-        const agents: { name: string, dischargeDate: Date, variant: string, status: boolean }[] = []
+        const agents: { name: string, dischargeDate: Date, variant: string }[] = []
         let str = '복무중인 인원이 없습니다'
         if (r) {
             r.forEach((doc) => {
                 agents.push({
                     name: doc.id,
                     dischargeDate: doc.data().dischargeDate.toDate(),
-                    variant: doc.data().variant,
-                    status: doc.data().status
+                    variant: doc.data().variant
                 })
             })
             if (agents) {
                 str = ''
                 agents.forEach((v) => {
-                    str = str +
+                    str +=
                         '\n\n' +
                         `${v.name}` +
                         '\n' +
-                        `'${v.variant} - ${v.status === true ? '현역' : '전역'}' | 전역일: ${format(v.dischargeDate, 'yyyy년 MM월 dd일')} | 잔여일수: ${formatDistanceToNowStrict(v.dischargeDate, { unit: 'day', locale: ko })}`
+                        `'+ ${v.variant} | 전역일: ${this.showDischargeDate(v.dischargeDate)} | 잔여일수: ${formatDistanceToNowStrict(v.dischargeDate, { unit: 'day', locale: ko })}`
                 })
-                return '```\n전역일 일람표' + str + '\n```'
+                return '```diff\n- 전역일 일람표' + str + '\n```'
             } else {
                 str = `SERVER ERROR`
                 return str
