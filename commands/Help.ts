1a3232f15aa5d6cbcc52c6ff6c27d115f9cc0b3a
Titancube <kbin0717@gmail.com>
2021-03-23 16:14:17 +0900

    fix: typo

--- commands/Help.ts
+++ commands/Help.ts
@@ -11,15 +11,13 @@ export abstract class Help {
     private async help(command: CommandMessage): Promise<void> {
         const { mTarget } = command.args
         const details = Client.getCommands();
-        const title = '**명령어 목록**\n\n`$도움 <명령어>` 를 입력해 특정 명령어에 대한 도움말을 확인할 수 있습니다.'
+        const title = '**명령어 목록**\n\n`$도움 <명령어>` 를 입력해 특정 명령어에 대한 도움말을 확인할 수 있습니다.\n\n'
         let str = ''
-        const opener = '```\n'
-        const closer = '\n```'
         if (!mTarget) {
             details.forEach(v => {
                 str = `${str} * ${v.infos.command} - ${v.infos.detail}\n`
             })
-            command.channel.send(title + opener + str + closer)
+            command.channel.send(title + str)
         } else {
             const result = details.filter(v => v.infos.command === mTarget.toString())
             if (result.length > 0) {
