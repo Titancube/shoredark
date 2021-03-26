b2b2b3a2946ec9b0f31a2b5f8da9c1d911870a3e
Titancube <kbin0717@gmail.com>
2021-03-26 16:58:57 +0900

    test: show amount

--- commands/Exchange.ts
+++ commands/Exchange.ts
@@ -3,21 +3,25 @@ import axios from 'axios'
 
 export abstract class Help {
 
-    @Command("환율 :baseCurrency :targetCurrency")
+    @Command("환율 :_amount :baseCurrency :targetCurrency")
     @Infos({
         command: `환율`,
-        detail: '`$환율 <기축통화> <대상통화>`',
+        detail: '`$환율 <금액> <기축통화> <대상통화>`',
         description: `* 유럽 중앙 은행 기준 매 시간 업데이트되는 전세계 환율을 확인할 수 있습니다.
-        * \`기축통화\` 를 별도로 설정하지 않을 시 대한민국 원(KRW)으로 자동 설정됩니다.
-        * \`대상통화\` 를 설정하면  `,
+* \`금액\` 을 별도로 설정하지 않을 시 1,000원을 기준으로 표시합니다.
+* \`기축통화\` 를 별도로 설정하지 않을 시 대한민국 원(KRW)으로 자동 설정됩니다.
+* \`대상통화\` 를 설정하면 해당 대상 통화를 가져옵니다.`,
     })
     private async help(command: CommandMessage): Promise<void> {
 
-        const { baseCurrency, targetCurrency } = command.args
+        const { _amount, baseCurrency, targetCurrency } = command.args
+        const amount = parseInt(_amount)
         const ucBaseCurrency: string = this.parseToString(baseCurrency).toUpperCase()
         const ucTargetCurrency: string = this.parseToString(targetCurrency).toUpperCase()
         const api = `https://api.exchangeratesapi.io/latest?${this.getBaseCurrency(ucBaseCurrency)}${this.getTargetCurrency(ucTargetCurrency)}`
         const r = await axios.get(api)
+
+        console.log(amount, baseCurrency, targetCurrency)
 
         if (r) {
             // let rates = ``
