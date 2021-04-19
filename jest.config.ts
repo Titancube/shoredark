b125b46b908c4864b6901fda5eeeba4ed34da1ee
Titancube <kbin0717@gmail.com>
2021-04-19 18:06:28 +0900

    add: jest configuration

---
+++ jest.config.ts
@@ -0,0 +1,28 @@
+import type { Config } from "@jest/types";
+
+const config: Config.InitialOptions = {
+  globals: {
+    "ts-jest": {
+      tsconfig: "tsconfig.json",
+      diagnostics: true,
+    },
+  },
+  preset: "ts-jest",
+  moduleNameMapper: {
+    "^@/(.*)$": "<rootDir>/$1",
+    "^~/(.*)$": "<rootDir>/$1",
+  },
+  moduleFileExtensions: ["ts", "js", "json"],
+  roots: ["<rootDir>"],
+  testMatch: ["**/__tests__/**/*.+(ts|js)", "**/?(*.)+(spec|test).+(ts|js)"],
+  transform: {
+    "^.+\\.(ts|tsx)$": "ts-jest",
+  },
+  collectCoverage: true,
+  collectCoverageFrom: [
+    "<rootDir>/commands/**/*.+(ts|js)",
+    "<rootDir>/discords/**/*.+(ts|js)",
+  ],
+};
+
+export default config;
