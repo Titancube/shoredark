ca040f3ff0150075b62211b92c7feb7808c3b017
Titancube <kbin0717@gmail.com>
2021-04-28 12:27:01 +0900

    add: Path

--- discords/DiscordApp.ts
+++ discords/DiscordApp.ts
@@ -2,7 +2,10 @@ import { CommandNotFound, Discord } from '@typeit/discord'
 import * as Path from 'path'
 
 @Discord('$', {
-  import: [Path.join(__dirname, '..', 'commands', '*.ts')],
+  import: [
+    Path.join(__dirname, '..', 'commands', '*.ts'),
+    Path.join(__dirname, '..', 'auto', '*.ts'),
+  ],
 })
 export class DiscordApp {
   @CommandNotFound()
