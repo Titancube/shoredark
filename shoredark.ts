a92f048cfc69da55af7f4c5207bb51b83b978cb2
Titancube <kbin0717@gmail.com>
2021-09-24 11:18:51 +0900

    fix: env being loaded as undefined

--- shoredark.ts
+++ shoredark.ts
@@ -1,24 +1,24 @@
-import { Client } from '@typeit/discord'
-import * as dotenv from 'dotenv'
-dotenv.config()
+import { Client } from "@typeit/discord";
+import * as dotenv from "dotenv";
+dotenv.config();
 
 export class Main {
-  private static _client: Client
+  private static _client: Client;
 
   static get Client(): Client {
-    return this._client
+    return this._client;
   }
 
   static start(): void {
-    this._client = new Client()
+    this._client = new Client();
     this._client.login(
-      process.env.CLIENT_TOKEN,
+      process.env.CLIENT_TOKEN!,
       `${__dirname}/discords/*.ts`,
       `${__dirname}/discords/*.js`
-    )
+    );
 
-    console.log('Shorelight bot is up and running!')
+    console.log("Shorelight bot is up and running!");
   }
 }
 
-Main.start()
+Main.start();
